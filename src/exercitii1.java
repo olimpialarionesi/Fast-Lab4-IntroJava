9b0a3960d6519052cefee1aec13b7c256dd8b17b
olimpialarionesi <olimpialarionesi@gmail.com>
2017-03-18 13:20:59 +0200

    am adaugat alte chestii

---
+++ src/exercitii1.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Olimpia on 3/18/2017.
+ */
+public class exercitii1 {
+    public static void main(String[] args) {
+        int numar;
+        numar = SkeletonJava.readIntGUI("Introduceti un numar");
+        if (numar >= 0) {
+            //aici voi calcula si afisa patratul sau
+        } else {
+            //aici voi afisa numarul asa cum l-am citit
+
+
+
+            }
+
+
+
+
+        }
+    }
